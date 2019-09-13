055864281a88f053ad45692b704182cc28e22bdd
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-09-13 11:22:51 +0200

    no show

--- net_test.py
+++ net_test.py
@@ -351,7 +351,7 @@ for epoch in range(1000):
 
         output = net(input)[0].detach().cpu().numpy()
 
-        map_gt = (target[0] * 255).astype(np.uint8)
+        map_gt = (target[0][0] * 255).astype(np.uint8)
         map_pred = (output[0][0] * 255).astype(np.uint8)
 
         # print("SHAPE", map_pred.shape)
