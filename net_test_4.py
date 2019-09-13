6dd9c7741b0c9b9f2aca87f4710a4532bd1bc4e1
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-09-13 23:43:30 +0200

    net simple

--- net_test_4.py
+++ net_test_4.py
@@ -280,7 +280,7 @@ class EpiDataset(Dataset):
 
         depth = cv2.imread(images[0], 2)
 
-        depth = depth / np.max(depth)
+        # depth = depth / np.max(depth)
 
         depth = np.expand_dims(depth, 0)
         return depth
