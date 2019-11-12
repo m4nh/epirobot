c4792118dcd17af812b89a138b4d3adf57d709be
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-11-11 18:49:23 -0700

    ciao

--- ano_test.py
+++ ano_test.py
@@ -152,7 +152,7 @@ class AnoEncoder(nn.Module):
     def forward(self, x, full_output=False):
         x = self.conv1(x)
         l1 = x
-        print("L!",l1.shape)
+
         x = self.downsample1(x)
         x = self.conv2(x)
         l2 = x
@@ -302,7 +302,7 @@ for epoch in range(5000):
                 input_low_features, output = model(input)
 
                 output_low_features = model.encoder(output)
-                print("FEATURES", input_low_features.shape, output_low_features.shape)
+                print("FEATURES", input_low_features.shape, output.shape)
 
                 input_r = torch.unsqueeze(input[:, 0, :, :], 1)
                 input_g = torch.unsqueeze(input[:, 1, :, :], 1)
