c3e47c26fd1e0b6202e2ac44d71edbede5cfe383
Daniele De Gregorio <degregorio.daniele@gmail.com>
2019-12-06 16:05:31 +0100

    ciao

---
+++ cmp_test.py
@@ -0,0 +1,217 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import importlib
+import os, glob
+
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchsummary
+from skimage import io, transform
+from skimage.transform import rescale
+import torch.optim as optim
+import cv2
+from torch.autograd import Variable
+from math import exp
+from kornia.losses import SSIM
+from torchvision import transforms, utils
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+from anomaleye import ElasticAE
+import argparse
+
+
+class CmpDataset0(Dataset):
+
+    def __init__(self, folder, is_test=False, crop=None, resize=256):
+        self.folder = folder
+        self.images = sorted(glob.glob(os.path.join(self.folder, '*')))
+        self.crop = crop
+        self.resize = resize
+
+        if not is_test:
+            self.tf = transforms.Compose(
+                [
+                    transforms.ToPILImage(),
+                    # transforms.Resize((resize, resize)),
+                    # transforms.Grayscale(),
+                    # transforms.RandomAffine(5, (0.05, 0.05), fillcolor=9),
+                    # torchvision.transforms.RandomHorizontalFlip(p=0.5),
+                    transforms.ToTensor()
+                ]
+            )
+        else:
+            self.tf = transforms.Compose(
+                [
+                    transforms.ToPILImage(),
+                    # transforms.Resize((resize, resize)),
+                    transforms.ToTensor()
+                ]
+            )
+
+    def __len__(self):
+        return len(self.images)
+
+    @staticmethod
+    def displayableImage(image):
+        img = image.permute(1, 2, 0)
+        img = img.cpu().numpy()
+        return np.uint8(img * 255.)
+
+    def __getitem__(self, idx):
+        # print(self.subfolders[idx])
+
+        img = Image.open(self.images[idx])
+        # print(img)
+        if self.crop is not None:
+            img = torchvision.transforms.functional.resized_crop(
+                img,
+
+                self.crop[0],
+                self.crop[1],
+                self.crop[2],
+                self.crop[3],
+                self.resize
+            )
+
+        img = torch.Tensor(np.float32(img) / 255.)
+        img = img.permute(2, 0, 1)
+
+        img = self.tf(img)
+        target_image = img.clone()
+
+        return {
+            'input': img,
+            'target': target_image
+        }
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", default="anomaleye_net", type=str)
+args = parser.parse_args()
+
+image_resize = 256
+input_channels = 3
+model = ElasticAE(args.name, image_resize, input_channels=input_channels, output_channels=input_channels,
+                  latent_size=500,
+                  layers=4,
+                  initial_filters=16, checkpoints_path='/tmp/anomaleye/')
+
+device = ("cuda:0" if torch.cuda.is_available() else "cpu")
+print("DEVICE:", device)
+model = model.to(device)
+torchsummary.summary(model, (input_channels, image_resize, image_resize))
+for param in model.parameters():
+    param.requires_grad = True
+
+# tensorboard
+writer = SummaryWriter(os.path.join("/tmp/anomaleye/runs", args.name))
+
+# OPTIMIZER
+lr = 0.001
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+crop_w = 1300 - 600
+dataset = CmpDataset0(folder='/tmp/train', crop=[500, 600, crop_w, crop_w], resize=image_resize)
+dataset_test = CmpDataset0(folder='/tmp/test', crop=[500, 600, crop_w, crop_w], is_test=True, resize=image_resize)
+
+# for d in dataset:
+#     img = dataset.displayableImage(d['input'])
+#     # 600,750,1300,1200
+#     cv2.imshow("image", img)
+#     cv2.waitKey(0)
+#     print(img.shape)
+
+# for d in dataset:
+#     img = d['input']
+#     img = dataset.displayableImage(img)
+#     cv2.imshow("image",img)
+#     cv2.waitKey(0)
+# dataset_neg = AnoDataset(folder='/tmp/ano_dataset_train_neg', is_negative=True)
+
+
+generator = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0, drop_last=False)
+generator_test = DataLoader(dataset_test, batch_size=8, shuffle=True, num_workers=0, drop_last=False)
+
+# LOAD MODEL IF ANY
+model.loadModel()
+
+LossL1 = nn.L1Loss()  # SSIM(11, reduction='mean')
+FeaturesLoss = nn.L1Loss()
+LossSSIM = SSIM(5, reduction='mean')
+
+for epoch in range(5000):
+
+    print("EPOCH", epoch)
+
+    if epoch % 10 == 0 and epoch > 0:
+        model.saveModel()
+
+    loss_ = 0.0
+    counter = 0.0
+    for gen in [generator]:  # , generator_neg]:
+        for index, batch in enumerate(gen):
+            model.train()
+            optimizer.zero_grad()
+
+            input = batch['input']
+            # print("SHAPE" * 10, input.shape)
+            input = input.to(device)
+
+            target = batch['target']
+            target = target.to(device)
+
+            with torch.set_grad_enabled(True):
+                output = model(input)
+
+                input_r = torch.unsqueeze(input[:, 0, :, :], 1)
+                input_g = torch.unsqueeze(input[:, 1, :, :], 1)
+                input_b = torch.unsqueeze(input[:, 2, :, :], 1)
+
+                output_r = torch.unsqueeze(output[:, 0, :, :], 1)
+                output_g = torch.unsqueeze(output[:, 1, :, :], 1)
+                output_b = torch.unsqueeze(output[:, 2, :, :], 1)
+
+                loss1 = LossL1(input, output)
+
+                loss2_r = LossSSIM(input_r, output_r)
+                loss2_g = LossSSIM(input_g, output_g)
+                loss2_b = LossSSIM(input_b, output_b)
+                loss2 = 0.3 * loss2_b + 0.3 * loss2_g + 0.3 * loss2_r
+
+                loss = loss1 + loss2  # + loss3
+
+                if index == len(gen) - 1 and epoch % 5 == 0:
+                    writer.add_scalar('Loss/reconstruction', loss1, epoch)
+                    writer.add_scalar('Loss/ssim', loss2, epoch)
+                    # writer.add_scalar('Loss/features', loss3, epoch)
+                    writer.add_image('Train/input_images', torchvision.utils.make_grid(input), epoch)
+                    writer.add_image('Train/reconstructed_images', torchvision.utils.make_grid(output), epoch)
+
+                loss.backward()
+                optimizer.step()
+
+                print("Batch: {}/{}".format(index, len(generator)))
+
+    if epoch % 5 == 0:
+        print("∞" * 20)
+        print("TEST " * 20)
+        for index, batch in enumerate(generator_test):
+            model.eval()
+            input = batch['input']
+            input = input.to(device)
+
+            output = model(input)
+
+            diff = torch.abs(input - output)
+
+            output = output.detach()
+
+            writer.add_image('Test/input_images', torchvision.utils.make_grid(input), epoch)
+            writer.add_image('Test/reconstructed_images', torchvision.utils.make_grid(output), epoch)
+            writer.add_image('Test/differences', torchvision.utils.make_grid(diff), epoch)
+            break
+
+writer.close()
---
+++ tgm_test.py
@@ -0,0 +1,77 @@
+import torchgeometry as tgm
+import numpy as np
+import cv2
+import torch
+
+reduce = 4.0
+cam0 = np.eye(4)
+cam1 = np.eye(4)
+cam0[:3, :3] = np.array([2826.171, 0, 1292.2 / reduce, 0, 2826.171, 965.806 / reduce, 0, 0, 1]).reshape((3, 3))
+cam1[:3, :3] = np.array([2826.171, 0, 1415.97 / reduce, 0, 2826.171, 965.806 / reduce, 0, 0, 1]).reshape((3, 3))
+
+T0 = np.eye(4)
+T1 = np.eye(4)
+T1[0, 3] = 178.089 / 1000.
+
+f0 = '/tmp/testepi/im0.png'
+f1 = '/tmp/testepi/im1.png'
+
+img0 = cv2.imread(f0)
+img1 = cv2.imread(f1)
+
+img0 = cv2.resize(img0, (int(img0.shape[1] / reduce), int(img0.shape[0] / reduce)))
+img1 = cv2.resize(img1, (int(img1.shape[1] / reduce), int(img1.shape[0] / reduce)))
+
+print(img0.shape)
+
+cam0 = torch.Tensor(cam0).unsqueeze(0)
+cam1 = torch.Tensor(cam1).unsqueeze(0)
+T0 = torch.Tensor(T0).unsqueeze(0)
+T1 = torch.Tensor(T1).unsqueeze(0)
+h = img0.shape[0]
+w = img0.shape[1]
+img_h = torch.Tensor(h)
+img_w = torch.Tensor(w)
+
+img_src = torch.Tensor(img0).permute(2, 0, 1).unsqueeze(0)
+img_dst = torch.Tensor(img1).permute(2, 0, 1).unsqueeze(0)
+print(h,w)
+# pinholes camera models
+pinhole_dst = tgm.PinholeCamera(intrinsics=cam1, extrinsics=T1, height=img_h, width=img_w)
+pinhole_src = tgm.PinholeCamera(intrinsics=cam0, extrinsics=T0, height=img_h, width=img_w)
+
+print(pinhole_src)
+
+depth_src = torch.nn.Parameter(torch.ones(1, 1, h, w))
+torch.nn.init.uniform_(depth_src)
+depth_src.requires_grad = True
+optimizer = torch.optim.Adam([depth_src], lr=0.1)
+L1 = torch.nn.L1Loss()
+
+warper = tgm.DepthWarper(pinhole_dst, h, w)
+warper.compute_projection_matrix(pinhole_src)
+
+for i in range(10000):
+    image_src = tgm.depth_warp(pinhole_dst, pinhole_src, depth_src, img_dst, h, w)  # NxCxHxW
+    loss = L1(image_src, img_src)
+
+    optimizer.zero_grad()
+    loss.backward(retain_graph=True)
+    optimizer.step()
+    print("loss ", loss)
+
+    depth = depth_src.squeeze().squeeze().detach().numpy()
+    depth = (depth - np.min(depth)) / (np.max(depth) - np.min(depth))
+    print(depth.shape, np.min(depth), np.max(depth))
+    depth = (depth*255).astype(np.uint8)
+    cv2.imshow("imaage", cv2.applyColorMap(depth, cv2.COLORMAP_JET))
+    cv2.waitKey(1)
+
+# pinhole_src = tgm.PinholeCamera(...)
+# >>> # create the depth warper, compute the projection matrix
+# >>> warper = tgm.DepthWarper(pinhole_dst, height, width)
+# >>> warper.compute_projection_matrix(pinhole_src)
+# >>> # warp the destionation frame to reference by depth
+# >>> depth_src = torch.ones(1, 1, 32, 32)  # Nx1xHxW
+# >>> image_dst = torch.rand(1, 3, 32, 32)  # NxCxHxW
+# >>> image_src = warper(depth_src, image_dst)  # NxCxHxW
